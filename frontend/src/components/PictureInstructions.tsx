e85117a37bc4ecccf0147496b949f6e2a754fc04
DeanToshi <0xdeantoshi@gmail.com>
2025-11-07 23:21:17 -0500

    pictureInstructions restyle

--- frontend/src/components/PictureInstructions.tsx
+++ frontend/src/components/PictureInstructions.tsx
@@ -9,25 +9,34 @@ function PictureInstructions() {
           <h2 className="picture-instructions-title">📸 Picture Instructions</h2>
         </div>
         <div className="picture-instructions-content">
-          <p className="picture-instructions-message">
-            You may choose to complete one of the following scenarios
-
-            Scenario 1:
-
-            Take a picture of the sweetpotatoes in the bin
-
-            Scenario 2:
-
-            Take the sweetpotatoes out of the box, laying them on the conveyor belt
-
-            Take a picture of the sweetpotatoes laid out on the conveyor belt
-
-            Requirements
-
-            Images must be in landscape orientation
-
-            Utilize the tape measure to make sure you are 6ft vertical distance from sweetpotatoes to camera
+          <p className="picture-instructions-intro">
+            You may choose to complete one of the following scenarios:
           </p>
+
+          <div className="scenarios-section">
+            <div className="scenario-card">
+              <h3 className="scenario-title">Scenario 1</h3>
+              <ul className="scenario-list">
+                <li>Take a picture of the sweetpotatoes in the bin</li>
+              </ul>
+            </div>
+
+            <div className="scenario-card">
+              <h3 className="scenario-title">Scenario 2</h3>
+              <ul className="scenario-list">
+                <li>Take the sweetpotatoes out of the box, laying them on the conveyor belt</li>
+                <li>Take a picture of the sweetpotatoes laid out on the conveyor belt</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="requirements-section">
+            <h3 className="requirements-title">Requirements</h3>
+            <ul className="requirements-list">
+              <li>Images must be in <strong>landscape orientation</strong> (phone screen sideways NOT up and down)</li>
+              <li>Utilize the tape measurer to make sure you are <strong>6ft vertical distance</strong> from sweetpotatoes to camera</li>
+            </ul>
+          </div>
         </div>
         <ScenarioToggle />
       </div>
